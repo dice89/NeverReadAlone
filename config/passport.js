0bd09f523d82435a50dd0f341cd6c99b8019cf89
Alexander Mueller <alexan.chr.mueller@gmail.com>
2014-10-25 21:35:37 +0200

    removed flash

--- config/passport.js
+++ config/passport.js
@@ -151,8 +151,7 @@ passport.use(new TwitterStrategy(secrets.twitter, function(req, accessToken, tok
   if (req.user) {
     User.findOne({ twitter: profile.id }, function(err, existingUser) {
       if (existingUser) {
-        req.flash('errors', { msg: 'There is already a Twitter account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
-        done(err);
+       done(err);
       } else {
         User.findById(req.user.id, function(err, user) {
           user.twitter = profile.id;
@@ -161,7 +160,6 @@ passport.use(new TwitterStrategy(secrets.twitter, function(req, accessToken, tok
           user.profile.location = user.profile.location || profile._json.location;
           user.profile.picture = user.profile.picture || profile._json.profile_image_url_https;
           user.save(function(err) {
-            req.flash('info', { msg: 'Twitter account has been linked.' });
             done(err, user);
           });
         });
@@ -178,9 +176,6 @@ passport.use(new TwitterStrategy(secrets.twitter, function(req, accessToken, tok
       user.profile.name = profile.displayName;
       user.profile.location = profile._json.location;
       user.profile.picture = profile._json.profile_image_url_https;
-
-
-      
       user.save(function(err) {
         done(err, user);
       });
