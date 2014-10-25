908b3585af0ec5e5e07c02ecf8b17c85ab572dd6
Alexander Mueller <alexan.chr.mueller@gmail.com>
2014-10-25 21:10:21 +0200

    added text index

--- models/User.js
+++ models/User.js
@@ -28,6 +28,7 @@ var userSchema = new mongoose.Schema({
 
 
 userSchema.index({ "geo" : "2dsphere"});
+userSchema.index({"expertise":"text"});
 /**
  * Hash the password for security.
  * "Pre" is a Mongoose middleware that executes before each user.save() call.
